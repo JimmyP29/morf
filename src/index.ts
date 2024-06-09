9b26907f2260a8f7d8fcef8dc3aa14e1be819a52
James Privett <jamesprivett29@gmail.com>
2024-06-09 16:49:32 +0100

    Initial Commander implementation working

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,12 @@
-function test() {
-  console.log('morf');
-}
-test();
+#!/usr/bin/env node
+import { program } from 'commander';
+
+program
+  .version('1.0.0')
+  .description('morf')
+  .option('-n, --name <type>', 'Add your name')
+  .action((options) => {
+    console.log(`Hey ${options.name}`);
+  });
+
+program.parse(process.argv);
