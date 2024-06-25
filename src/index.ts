6a5ebb2f571dccb37c41e1dfc74e08c043a743df
James Privett <jamesprivett29@gmail.com>
2024-06-25 21:01:14 +0100

    createFileAtDestination function working

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 #!/usr/bin/env node
 import fs from 'fs';
 import path from 'path';
+import util from 'util';
+import stream from 'stream';
+
+const finished = util.promisify(stream.finished);
 
 console.log('Working...');
 
-const getDataFromFile = async (pathToFile: string) => {
+const getDataFromOriginFile = async (pathToFile: string) => {
   const ext = path.extname(pathToFile);
-  console.log('EXT: ', ext);
 
   if (!ext) throw Error('Please provide a valid file (.json or .csv)');
 
@@ -17,7 +20,7 @@ const getDataFromFile = async (pathToFile: string) => {
         console.error('ERROR: ', err);
       })
       .on('data', (chunk) => {
-        chunks.push(chunk);
+        chunks.push({ chunk: chunk, type: ext });
       })
       .on('end', () => {
         resolve(chunks);
@@ -25,8 +28,38 @@ const getDataFromFile = async (pathToFile: string) => {
   });
 };
 
-const jsonFile = await getDataFromFile('./test-data/dummy1.json');
-const csvFile = await getDataFromFile('./test-data/dummy1.csv');
+const createFileAtDestination = async (
+  file: { chunk: any; type: string }[],
+  destination: string,
+) => {
+  if (!file || file.length === 0) throw Error('No file found');
 
-console.log('JSON FILE: ', jsonFile);
-console.log('CSV FILE: ', csvFile);
+  try {
+    if (!fs.existsSync(destination)) {
+      fs.mkdirSync(destination);
+      const folder = path.basename(destination);
+      const parentFolder = path.dirname(destination);
+      console.log(`New folder - '${folder}' created at ${parentFolder}`);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+
+  const { chunk, type } = file[0];
+
+  const writable = fs.createWriteStream(`${destination}/test${type}`, {
+    encoding: 'utf-8',
+  });
+
+  writable.write(chunk);
+  writable.end();
+
+  await finished(writable);
+
+  console.log(`File (test${type}) created at ${destination}`);
+};
+
+const jsonFile = await getDataFromOriginFile('./test-data/origin/dummy1.json');
+const csvFile = await getDataFromOriginFile('./test-data/origin/dummy1.csv');
+
+createFileAtDestination(jsonFile as [], './test-data/destination');
