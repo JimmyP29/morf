dd941942fb57a1d08d995ca1a05027fa9814f8c6
James Privett <jamesprivett29@gmail.com>
2024-07-07 15:37:45 +0100

    aggregateJSONToCSV end to end

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import fs from 'fs';
 import path from 'path';
 import util from 'util';
 import stream from 'stream';
+import { stringify } from 'querystring';
 
 const finished = util.promisify(stream.finished);
 
@@ -163,7 +164,132 @@ const convertFile = async (
   );
 };
 
-const aggregateJSONToCSV = (from: string[], destinantion: string) => {};
+const standardiseJSONValues = async (
+  from: string,
+  value: string | number | boolean,
+) => {
+  const file = await getDataFromOriginFile(from);
+  // const fileName = path.basename(fromPath).split('.')[0];
+  console.log(JSON.stringify(file[0].chunk));
+  const jsonChunk = JSON.parse(file[0].chunk);
+  const copy = jsonChunk;
+  console.log('COPY: ', copy);
+  // Instead of relying on the first key being called 'data' this needs to be abstracted out and referenced below.
+  const zeroedValues = copy.data.map((entry: any) => {
+    let zeroedEntry = {};
+    Object.keys(entry).forEach((key) => {
+      zeroedEntry = { ...zeroedEntry, [key]: value };
+    });
+
+    return zeroedEntry;
+  });
+
+  return zeroedValues;
+};
+
+const isDeepEqual = (object1: any, object2: any) => {
+  console.log('OBJ 1: ', object1);
+  console.log('OBJ 2: ', object2);
+  const objKeys1 = Object.keys(object1);
+  const objKeys2 = Object.keys(object2);
+
+  if (objKeys1.length !== objKeys2.length) return false;
+
+  for (var key of objKeys1) {
+    const value1 = object1[key];
+    const value2 = object2[key];
+
+    const isObjects = isObject(value1) && isObject(value2);
+
+    if (
+      (isObjects && !isDeepEqual(value1, value2)) ||
+      (!isObjects && value1 !== value2)
+    ) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const isObject = (object: any) => {
+  return object != null && typeof object === 'object';
+};
+
+const glueJSON = (objects: object[]): any => {
+  //This doesn't work fully - it needs to take into account objects and arrays, not just arrays
+  return { ...objects.map((o) => o) };
+};
+
+const aggregateJSONToCSV = async (
+  from: string[],
+  destination: string,
+): Promise<void> => {
+  if (from.length === 0)
+    throw Error(
+      `${RED}Please provide at least one origin path within 'from'.${RESET}`,
+    );
+
+  const originJSON: any[] = [];
+  const comparison: any[] = [];
+
+  from.forEach(async (fromPath) => {
+    const json = await getDataFromOriginFile(fromPath);
+    if (!json) throw Error(`${RED}Unable to use JSON file (${json}).${RESET}`);
+
+    originJSON.push(JSON.parse(json[0].chunk));
+  });
+
+  if (from.length > 1) {
+    Promise.all(
+      from.map((fromPath) => {
+        return standardiseJSONValues(fromPath, '');
+      }),
+    ).then(async (standardisedJSON) => {
+      comparison.push(standardisedJSON);
+
+      if (!comparison || !comparison[0]) {
+        throw Error(`${RED}No files to compare${RESET}`);
+      }
+
+      console.log('COMPARISON: ', comparison);
+      console.log(`${CYAN}%s${RESET}`, 'Starting Recursion...');
+      if (isDeepEqual(comparison[0][0], comparison[0][1])) {
+        console.log(
+          `${GREEN}%s${RESET}`,
+          `All JSON file structures match - Good Job!`,
+        );
+        console.log('ORIGIN JSON: ', originJSON);
+        const glued = glueJSON([...originJSON[0].data, ...originJSON[1].data]);
+
+        console.log('GLUED: ', glued);
+        await createFileAtDestination(
+          [{ chunk: JSON.stringify(glued), type: FileType.CSV }],
+          'test-aggregated-csv',
+          destination,
+        );
+      } else {
+        console.error(
+          `${RED}All JSON files provided have to have the same structure, please check and try again.${RESET}`,
+        );
+      }
+    });
+  } else {
+    // No comparisson checks required - just createFileAtDestination
+    await createFileAtDestination(
+      [{ chunk: JSON.stringify(originJSON), type: FileType.CSV }],
+      'test-aggregated-csv',
+      destination,
+    );
+  }
+};
+// aggregateJSONToCSV(
+//   ['./test-data/origin/dummy1.json'],
+//   './test-data/destination/aggregated',
+// );
+aggregateJSONToCSV(
+  ['./test-data/origin/dummy1.json', './test-data/origin/dummy1.json'],
+  './test-data/destination/aggregated',
+);
 
 // const jsonFile = await getDataFromOriginFile('./test-data/origin/dummy1.json');
 // const csvFile = await getDataFromOriginFile('./test-data/origin/dummy1.csv');
@@ -171,31 +297,7 @@ const aggregateJSONToCSV = (from: string[], destinantion: string) => {};
 // createFileAtDestination(jsonFile as [], './test-data/destination');
 
 // convertFile(
-//   './test-data/origin/dummy1.json',
-//   '.csv',
-//   './test-data/destination/converted',
-// );
-// convertFile(
-//   './test-data/origin/people-100.csv',
-//   '.json',
-//   './test-data/destination/converted',
-// );
-convertFile(
-  './test-data/origin/dummy1.csv',
-  FileType.JSON,
-  './test-data/destination/converted',
-);
-
-// setTimeout(() => {
-//   convertFile(
-//     './test-data/destination/converted/test.csv',
-//     '.json',
-//     './test-data/destination/converted',
-//   );
-// }, 1000);
-
-// convertFile(
-//   './test-data/destination/converted/test.json',
-//   '.csv',
+//   './test-data/origin/dummy1.csv',
+//   FileType.JSON,
 //   './test-data/destination/converted',
 // );
